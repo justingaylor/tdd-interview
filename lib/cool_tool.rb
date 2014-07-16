b0efde5bdb72c246bed31134f6ab2cd149c2fc78
Christian Teijon <christian@crunis.net>
2014-07-16 20:07:27 +0200

    All tests passed for cool_tool

--- lib/cool_tool.rb
+++ lib/cool_tool.rb
@@ -1,2 +1,17 @@
 
 # Code here
+module RightScale
+  class CoolTool
+    DEFAULT_FILEPATH = "/var/log/cool_tool/cool_tool.log"
+    attr_reader :file_path
+
+    def initialize(file_path = DEFAULT_FILEPATH)
+      @file_path = file_path
+    end
+
+    def expanded_file_path
+      File.expand_path(file_path)
+    end
+  end
+end
+
